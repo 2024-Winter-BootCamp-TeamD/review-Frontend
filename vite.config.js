c1b9e75429e6f4af70987c46c2fbc7651f7e222f
Lauiee <132264578+Lauiee@users.noreply.github.com>
2025-01-10 04:40:27 +0900

    test: 익스텐션 적용위한 코드 추가

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: "dist", // 익스텐션 파일이 출력될 폴더
+    rollupOptions: {
+      input: {
+        popup: "index.html", // action에 사용할 HTML 파일
+      },
+    },
+  },
+});

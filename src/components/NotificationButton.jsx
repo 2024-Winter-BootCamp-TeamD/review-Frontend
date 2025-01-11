d74b0988f437bb6bed60ece6c2d523db1a9da085
Nekerworld <chrisabc94@gmail.com>
2025-01-11 16:53:22 +0900

    feat: 알림버튼 생성

--- src/components/NotificationButton.jsx
+++ src/components/NotificationButton.jsx
@@ -1,2 +1,38 @@
-import { IoIosNotifications } from "react-icons/io";
+import React from 'react';
+import PropTypes from 'prop-types';
+import { IoMdNotifications } from "react-icons/io";
 
+const NotificationButton = ({ hasNotification }) => {
+  const styles = {
+    button: {
+      position: 'fixed',
+      top: '50px',
+      right: '50px',
+    },
+    icon: {
+      fontSize: '52px',
+      color: '#191A23',
+    },
+    dot: {
+      position: 'absolute',
+      top: '6px',
+      right: '8px',
+      width: '15px',
+      height: '15px',
+      backgroundColor: '#FF715B',
+      borderRadius: '50%',
+    },
+  };
+
+  return (
+    <div style={styles.button}>
+      <IoMdNotifications style={styles.icon} />
+      {hasNotification && <div style={styles.dot}></div>}
+    </div>
+  );
+};
+NotificationButton.propTypes = {
+  hasNotification: PropTypes.bool.isRequired,
+};
+
+export default NotificationButton;

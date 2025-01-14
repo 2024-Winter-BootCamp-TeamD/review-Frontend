bd61ea062cde69f58df0032b66715e6a384793ab
Nekerworld <chrisabc94@gmail.com>
2025-01-14 21:17:12 +0900

    대시보드에 맞춰 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Dashboard from "./pages/Dashboard.jsx";
-import History from "./pages/History.jsx";
-import Repositories from "./pages/Repositories.jsx";
-import Report from "./pages/Report.jsx";
 import Sidebar from "./components/Sidebar/Sidebar.jsx";
-
-import "./App.css";
 import NotificationButton from "./components/NotificationButton/NotificationButton.jsx";
+import Dashboard from "./pages/Dashboard.jsx";
+import "./App.css";
 
 function App() {
   return (
@@ -17,9 +13,6 @@ function App() {
         <div className="content">
           <Routes>
             <Route path="/" element={<Dashboard />} />
-            <Route path="/history" element={<History />} />
-            <Route path="/repositories" element={<Repositories />} />
-            <Route path="/report" element={<Report />} />
           </Routes>
         </div>
         <NotificationButton />

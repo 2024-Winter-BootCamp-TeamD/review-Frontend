2df4bc81938eeff446b53c8bf3e575bba59fca21
Lauiee <132264578+Lauiee@users.noreply.github.com>
2025-01-14 16:19:40 +0900

    env: 환경 맞추기

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,24 +1,24 @@
-import React, { useState } from 'react';
-import './Sidebar.css';
-import SidebarButton from '../SidebarButton/SidebarButton.jsx';
-import { useNavigate } from 'react-router-dom';
+import React, { useState } from "react";
+import "./Sidebar.css";
+import SidebarButton from "../SidebarButton/SidebarButton.jsx";
+import { useNavigate } from "react-router-dom";
 
 const Sidebar = () => {
-  const buttons = ['Dashboard', 'History', 'Repositories', 'Report'];
-  const [activeButton, setActiveButton] = useState('Dashboard');
+  const buttons = ["Dashboard", "History", "Repositories", "Report"];
+  const [activeButton, setActiveButton] = useState("Dashboard");
   const navigate = useNavigate();
 
   const handleButtonClick = (label) => {
     setActiveButton(label);
     // 라우팅 경로 매핑
-    if (label === 'Dashboard') {
-      navigate('/');
-    } else if (label === 'History') {
-      navigate('/history');
-    } else if (label === 'Repositories') {
-      navigate('/repositories');
-    } else if (label === 'Report') {
-      navigate('/report');
+    if (label === "Dashboard") {
+      navigate("/");
+    } else if (label === "History") {
+      navigate("/history");
+    } else if (label === "Repositories") {
+      navigate("/repositories");
+    } else if (label === "Report") {
+      navigate("/report");
     }
     console.log(`${label} button clicked!`);
   };

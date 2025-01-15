92c5ceba657be7a9dfd0d046cf5f90875ec9ee70
Nekerworld <chrisabc94@gmail.com>
2025-01-16 02:32:57 +0900

    feat: 버튼 디자인 정상화

--- src/components/ModeSwitchButton/ModeSwitchButton.jsx
+++ src/components/ModeSwitchButton/ModeSwitchButton.jsx
@@ -1,23 +1,41 @@
 //src/components/ModeSwitchButton/ModeSwitchButton.jsx
 
-import React from "react";
-import "./ModeSwitchButton.css";
+import React, { useState, useEffect } from 'react';
+import './ModeSwitchButton.css';
 
-const ModeSwitchButton = () => {
+const DarkMode = () => {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  useEffect(() => {
+    const savedState = localStorage.getItem('toggleState') === 'true';
+    setIsDarkMode(savedState);
+    document.body.classList.toggle('dark-mode', savedState);
+  }, []);
+
+  const handleToggle = () => {
+    const newState = !isDarkMode;
+    setIsDarkMode(newState);
+    localStorage.setItem('toggleState', newState);
+    document.body.classList.toggle('dark-mode', newState);
+  };
+
   return (
-    <div className="mode-switch-button">
-      <div className="wrapper">
-        <div className="toggle">
-          <input className="toggle-input" type="checkbox" />
-          <div className="toggle-bg"></div>
-          <div className="toggle-switch">
-            <div className="toggle-switch-figure"></div>
-            <div className="toggle-switch-figureAlt"></div>
-          </div>
+    <div className="wrapper">
+      <div className="toggle">
+        <input
+          className="toggle-input"
+          type="checkbox"
+          checked={isDarkMode}
+          onChange={handleToggle}
+        />
+        <div className="toggle-bg"></div>
+        <div className="toggle-switch">
+          <div className="toggle-switch-figure"></div>
+          <div className="toggle-switch-figureAlt"></div>
         </div>
       </div>
     </div>
   );
 };
 
-export default ModeSwitchButton;
+export default DarkMode;

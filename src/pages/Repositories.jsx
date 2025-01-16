8efbe5cc547ca54c689987e1198c4838a65b84a3
Nekerworld <chrisabc94@gmail.com>
2025-01-16 11:09:44 +0900

    feat: Repositories 다크모드 적용 완료, 대시보드 모드선택버튼 텍스트 색 정상화

--- src/pages/Repositories.jsx
+++ src/pages/Repositories.jsx
@@ -12,18 +12,16 @@ import BugReportIcon from "@mui/icons-material/BugReport";
 const RepositoryContainer = styled.div`
   width: 46%;
   height: 57rem;
-  background-color: #f0f0f0;
+  background-color: ${({ isDarkMode }) => (isDarkMode ? '#00000050' : '#f0f0f0')};
   border-radius: 20px;
-  background: #fff;
-  box-shadow:
-    0px 4px 8px 3px rgba(0, 0, 0, 0.15),
-    0px 1px 3px 0px rgba(0, 0, 0, 0.3);
+  box-shadow: 0px 4px 8px 3px rgba(0, 0, 0, 0.15), 0px 1px 3px 0px rgba(0, 0, 0, 0.3);
   padding: 20px;
   overflow: hidden;
+  border: ${({ isDarkMode }) => (isDarkMode ? '1px solid #FFFFFF' : 'none')};
 `;
 
 const RepositoryTitle = styled.h1`
-  color: #000;
+  color: ${({ isDarkMode }) => (isDarkMode ? '#FFFFFF' : '#000000')};
   text-align: center;
   font-family: Poppins;
   font-size: 30px;
@@ -57,16 +55,21 @@ const RepositoryWrapper = styled.div`
   margin-top: 20px;
   overflow-y: auto;
 
-  /* 스크롤바 스타일링 (선택사항) */
+  /* 스크롤바 스타일링 */
   &::-webkit-scrollbar {
-    width: 8px;
-  }
-  &::-webkit-scrollbar-thumb {
-    background-color: #d9d9d9;
-    border-radius: 4px;
+    width: 12px; /* 스크롤바 너비 */
   }
   &::-webkit-scrollbar-track {
-    background-color: #f0f0f0;
+    background: ${({ isDarkMode }) => (isDarkMode ? '#4A4A4A' : '#D9D9D9')}; /* 트랙 배경색 */
+    border-radius: 10px; /* 둥근 모서리 */
+  }
+  &::-webkit-scrollbar-thumb {
+    background-color: ${({ isDarkMode }) => (isDarkMode ? '#FFFFFF' : '#777777')}; /* 스크롤바 색상 */
+    border-radius: 10px; /* 둥근 모서리 */
+    border: 3px solid ${({ isDarkMode }) => (isDarkMode ? '#333' : '#f0f0f0')}; /* 스크롤바와 트랙 사이의 간격 */
+  }
+  &::-webkit-scrollbar-thumb:hover {
+    background-color: ${({ isDarkMode }) => (isDarkMode ? '#c7c7c7' : '#555')}; /* 호버 시 스크롤바 색상 */
   }
 `;
 
@@ -75,13 +78,15 @@ const RepositoryItem = styled.div`
   min-height: 85px;
   margin: 20px auto;
   padding: 15px;
-  background: #ffffff;
+  background: ${({ isDarkMode }) => (isDarkMode ? '#00000050' : '#ffffff')};
+  border: ${({ isDarkMode }) => (isDarkMode ? '1px solid #FFFFFF' : 'none')};
   border-radius: 20px;
   box-shadow: 0px 2px 5px 0px rgba(0, 0, 0, 0.5);
   display: flex;
   align-items: center;
   gap: 15px;
   position: relative;
+  color: ${({ isDarkMode }) => (isDarkMode ? '#FFFFFF' : '#000000')};
 `;
 
 const RepoImage = styled.img`
@@ -195,7 +200,7 @@ const Language = styled.span`
   }
 `;
 
-const Repositories = () => {
+const Repositories = ({ isDarkMode }) => {
   const [unselectedRepos, setUnselectedRepos] = useState([
     {
       id: 1,
@@ -294,17 +299,17 @@ const Repositories = () => {
 
   return (
     <RepositoriesWrapper>
-      <RepositoryContainer>
-        <RepositoryTitle>Unselected</RepositoryTitle>
+      <RepositoryContainer isDarkMode={isDarkMode}>
+        <RepositoryTitle isDarkMode={isDarkMode}>Unselected</RepositoryTitle>
         <SearchBarWrapper>
           <SearchBar
             value={searchTermUnselected}
             onChange={(e) => setSearchTermUnselected(e.target.value)}
           />
         </SearchBarWrapper>
-        <RepositoryWrapper>
+        <RepositoryWrapper isDarkMode={isDarkMode}>
           {filteredUnselectedRepos.map((repo) => (
-            <RepositoryItem key={repo.id}>
+            <RepositoryItem key={repo.id} isDarkMode={isDarkMode}>
               <RepoImage src={repo.image} alt="Repository thumbnail" />
               <RepoContent>
                 <RepoHeader>
@@ -337,17 +342,17 @@ const Repositories = () => {
         </RepositoryWrapper>
       </RepositoryContainer>
 
-      <RepositoryContainer>
-        <RepositoryTitle>Selected</RepositoryTitle>
+      <RepositoryContainer isDarkMode={isDarkMode}>
+        <RepositoryTitle isDarkMode={isDarkMode}>Selected</RepositoryTitle>
         <SearchBarWrapper>
           <SearchBar
             value={searchTermSelected}
             onChange={(e) => setSearchTermSelected(e.target.value)}
           />
         </SearchBarWrapper>
-        <RepositoryWrapper>
+        <RepositoryWrapper isDarkMode={isDarkMode}>
           {filteredSelectedRepos.map((repo) => (
-            <RepositoryItem key={repo.id}>
+            <RepositoryItem key={repo.id} isDarkMode={isDarkMode}>
               <RepoImage src={repo.image} alt="Repository thumbnail" />
               <RepoContent>
                 <RepoHeader>

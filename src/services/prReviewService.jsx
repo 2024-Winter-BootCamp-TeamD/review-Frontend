1dda6c84dde1f88947b84066fbbf771b2fe7d46a
Lauiee <132264578+Lauiee@users.noreply.github.com>
2025-01-21 16:35:02 +0900

    feat: history페이지도 userId 동적 사용 변경

--- src/services/prReviewService.jsx
+++ src/services/prReviewService.jsx
@@ -1,8 +1,23 @@
 import api from "./api";
 
-// PR 리뷰 전체 조회
-export const getPRReviews = async (user_id, page, size = 20) => {
+// 사용자 정보 가져오기
+export const fetchUserInfo = async () => {
   try {
+    const userInfo = await chrome.storage.local.get("userInfo");
+    const user_id = userInfo.userInfo?.id;
+    console.log("로그인 된 사용자 id:", user_id);
+    if (!user_id) {
+      throw new Error("로그인된 사용자 정보를 찾을 수 없습니다.");
+    }
+  }
+};
+
+// PR 리뷰 전체 조회
+export const getPRReviews = async (page, size = 20) => {
+  try {
+    const userInfo = await fetchUserInfo();
+    const user_id = userInfo.user_details.id;
+
     const response = await api.get("/pr-reviews/", {
       params: {
         user_id,
@@ -18,8 +33,11 @@ export const getPRReviews = async (user_id, page, size = 20) => {
 };
 
 // PR 리뷰 검색
-export const searchPRReviews = async (user_id, title, page, size = 20) => {
+export const searchPRReviews = async (title, page, size = 20) => {
   try {
+    const userInfo = await fetchUserInfo();
+    const user_id = userInfo.user_details.id;
+
     if (!title) {
       throw new Error("검색어를 입력해주세요.");
     }
@@ -40,8 +58,11 @@ export const searchPRReviews = async (user_id, title, page, size = 20) => {
 };
 
 // PR 모드 카테고리별 통계 조회
-export const getPRReviewCategories = async (user_id) => {
+export const getPRReviewCategories = async () => {
   try {
+    const userInfo = await fetchUserInfo();
+    const user_id = userInfo.user_details.id;
+
     const response = await api.get("/pr-reviews/category", {
       params: {
         user_id,
